32d2ab17fe473e1e953f3985b894dacf6a3e455c
Ertan Turan <ertanturan@outlook.com>
2024-07-28 19:06:16 +0100

    automatically move the scriptable object to its correct directory

--- Runtime/CustomTools/LocalizationTool/Scripts/Language/Language.cs
+++ Runtime/CustomTools/LocalizationTool/Scripts/Language/Language.cs
@@ -1,18 +1,46 @@
-﻿using TMPro;
+﻿using System.IO;
+using System.Threading;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(
-    fileName = "New Language",
-    menuName = "Localization/Language")]
+	                fileName = "New Language",
+	                menuName = "Localization/Language")]
 public class Language : ScriptableObject
 {
-    public Alphabet Alphabet;
+	public Alphabet Alphabet;
 
-    public string Name;
+	public string Name;
 
-    public Sprite Icon;
+	public Sprite Icon;
 
-    public SystemLanguage systemLanguageType;
+	public SystemLanguage systemLanguageType;
 
-    public TMP_FontAsset LanguageFontAsset;
+	public TMP_FontAsset LanguageFontAsset;
+
+	private void Awake()
+	{
+	#if UNITY_EDITOR
+		WaitAndMove();
+	#endif
+	}
+
+
+	private void WaitAndMove()
+	{
+		Thread.Sleep(10);
+		string oldPath = AssetDatabase.GetAssetPath(this);
+		string dir = Path.Combine(Directory.GetCurrentDirectory(), "Assets/Resources/Data/Localization/Languages");
+
+		if (!Directory.Exists(dir))
+		{
+			Directory.CreateDirectory(dir);
+		}
+
+		AssetDatabase.Refresh();
+
+		string targetPath = $"Assets/Resources/Data/Localization/Languages/{name}.asset";
+		AssetDatabase.MoveAsset(oldPath, targetPath);
+	}
 }
