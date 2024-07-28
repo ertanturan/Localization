55800dd49da7923e9feb5f1dd55071ef520144eb
Ertan Turan <ertanturan@outlook.com>
2024-07-28 18:49:06 +0100

    fixed the demo scene

--- Runtime/CustomTools/LocalizationTool/Scripts/Language/LanguageDependentVideo.cs
+++ Runtime/CustomTools/LocalizationTool/Scripts/Language/LanguageDependentVideo.cs
@@ -6,39 +6,47 @@ using UnityEngine.Video;
 [Serializable]
 public struct LanguageVideoClipTuple
 {
-    public Language Language;
-    public VideoClip VideoClip;
+	public Language Language;
+	public VideoClip VideoClip;
 }
+
 [CreateAssetMenu(
-    fileName = "New Language Dependent Video",
-    menuName = "Localization/Language Dependent Video")]
+	                fileName = "New Language Dependent Video",
+	                menuName = "Localization/Language Dependent Video")]
 public class LanguageDependentVideo : ScriptableObject
 {
-    public LanguageVideoClipTuple[] videoClips;
+	public LanguageVideoClipTuple[] videoClips;
 
-    private Dictionary<Language, VideoClip> _dictionary;
+	private Dictionary<Language, VideoClip> _dictionary;
 
-    private Dictionary<Language, VideoClip> Dictionary
-    {
-        get
-        {
-            if (_dictionary == null)
-            {
-                _dictionary = new Dictionary<Language, VideoClip>();
-                foreach (var item in videoClips)
-                {
-                    if (item.Language == null)
-                        Debug.LogError("Language empty for Language Dependent Video: " + this);
-                    else
-                        _dictionary[item.Language] = item.VideoClip;
-                }
-            }
-            return _dictionary;
-        }
-    }
+	private Dictionary<Language, VideoClip> Dictionary
+	{
+		get
+		{
+			if (_dictionary == null)
+			{
+				_dictionary = new Dictionary<Language, VideoClip>();
+				foreach (var item in videoClips)
+				{
+					if (item.Language == null)
+						Debug.LogError("Language empty for Language Dependent Video: " + this);
+					else
+						_dictionary[item.Language] = item.VideoClip;
+				}
+			}
 
-    public VideoClip GetVideoClip(Language language)
-    {
-        return Dictionary[language];
-    }
+			return _dictionary;
+		}
+	}
+
+	public VideoClip GetVideoClip(Language language)
+	{
+		if (Dictionary.ContainsKey(language))
+		{
+			return Dictionary[language];
+		}
+
+		Debug.LogError($"No video was found for {language.name} language.. Assigning null");
+		return null;
+	}
 }
